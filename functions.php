40d99a819c2a598e4c85f0a0ce6c8de217a59d84
Danielcresp <dneli6cres@outlook.com>
2019-06-13 20:57:58 -0600

    iconos, menu del header y librerias css

--- functions.php
+++ functions.php
@@ -3,11 +3,17 @@
 function sympolys_styles(){
  //Registrar estilos CSS
  wp_register_style('style',get_stylesheet_directory_uri().'/style.css');
+ wp_register_style('bootstrap',get_stylesheet_directory_uri().'/assets/css/bootstrap.min.css');
+ wp_register_style('normalize',get_stylesheet_directory_uri().'/assets/css/normalize.css');
+ wp_register_style('fontw',get_stylesheet_directory_uri().'/assets/css/font_awesome.css');
  //Registrar Scripts JS
 //  wp_enqueue_script('fluidbox',get_template_directory_uri().'/js/jquery.fluidbox.js',array('jquery'),'1.0.0',true);
 
  //Obtener CSS
- wp_enqueue_style('style'); 
+ wp_enqueue_style('style');
+ wp_enqueue_style('bootstrap');
+ wp_enqueue_style('normalize');
+ wp_enqueue_style('fontw');
 
  //Obtener JS
  wp_enqueue_script('jquery'); 
--- header.php
+++ header.php
@@ -4,12 +4,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- icono -->
+    <link rel="shortcut icon" href="/assets/icono/synfonypoly.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/icono/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/icono/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/icono/favicon-16x16.png">
+    <link rel="manifest" href="assets/icono/site.webmanifest">
+    <link rel="mask-icon" href="assets/icono/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="assets/icono/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="assets/icono/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <!-- fin icono -->
     <?php  wp_head(); ?> 
     <title>Sympolys</title>
 </head>
 <body <?php body_class(); ?>>
-<header>
-
+<header class="container">
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <a href="#" class="navbar-brand">
+        <img class="img-logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/Synfonypoly.png" alt="imagen">
+    </a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div id="navbarCollapse" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a href="#" class="nav-link">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Porfolio</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Shop</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Page</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Paginas</a>
+                <div class="dropdown-menu">
+                    <a href="#" class="dropdown-item">Contacto</a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#"class="dropdown-item">Contacto</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Contacto</a>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+                <a href=""><span class="fas fa-shopping-cart"></span></a>
+                <a href=""><span class="fas fa-search"></span></a>
+            </li>
+        </ul>
+    </div>
+</nav>
 </header>
     
 
